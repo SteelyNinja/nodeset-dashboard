4db9cc62a74747c127a2abc3c45394bd1b54e076
SteelyNinja <gary@garypower.com>
2025-06-15 07:07:46 +0000

    delete file

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-NodeSet Validator Monitor Dashboard
-Entry point for the Streamlit application
-"""
-
-if __name__ == "__main__":
-    from dashboard import run_dashboard
-    run_dashboard()
